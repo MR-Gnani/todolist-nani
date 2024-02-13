e81b72aa18f7f38d570216285512bf5337f53322
nani kim <flsk9698@naver.com>
2024-02-13 16:33:25 +0900

    complete랜더링완

--- main.js
+++ main.js
@@ -1,21 +1,50 @@
 let taskInput = $(`#task-input`);
 
-$(`#add-Button`).on("click", render);
+$(`#add-Button`).on("click", complete);
+$('#task-input').keypress(function(e) { // Enter키 누르면 render함수 호출
+    if (e.which === 13) { 
+        complete();
+    }
+});
 
-function render(){
-   let taskContent = taskInput.val();
-   let taskItem = getTaskItem(taskContent);
-   $("#task-board").append(taskItem);
+function randomIdGenerate(){
+    return '_' + Math.random().toString(36).substring(2,9)
 }
 
-function getTaskItem(taskContent){
+function complete(){
+   let task = {
+        id: randomIdGenerate(),
+        taskContent: taskInput.val()
+   } 
+   console.log(task);
+   let taskItem = getTaskItem(task);
+   $("#task-board").append(taskItem);
+   taskInput.val("");
+}
+
+function getTaskItem(task){
     let item=
-    `<div class="task">
-         <span>${taskContent}</span>
+    `<div class="task" id="taskItem-${task.id}">
+         <span>${task.taskContent}</span>
          <div class="button-box">
-             <button> Check </button>
-             <button> Delete </button>
+             <button onclick="toggleComplete('${task.id}')"> Check </button>
+             <button onclick="deleteTask()"> Delete </button>
          </div>
      </div>`
      return item;
+}
+
+function toggleComplete(id){
+    
+    let Status = $(`#taskItem-${id}`);
+
+    if(Status.hasClass("task-done")){ 
+        Status.removeClass("task-done");
+    } else {
+        Status.addClass("task-done");
+    }
+}
+
+function deleteTask(){
+    console.log("삭제하다");
 }
