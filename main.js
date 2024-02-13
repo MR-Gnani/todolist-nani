d8524282af2fd9dbc75484437820c16a4213c906
nani kim <flsk9698@naver.com>
2024-02-13 16:46:10 +0900

    함수이름 수정

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 let taskInput = $(`#task-input`);
 
-$(`#add-Button`).on("click", complete);
+// 버튼 클릭 시 render함수 호출
+$(`#add-Button`).on("click", render);
 $('#task-input').keypress(function(e) { // Enter키 누르면 render함수 호출
     if (e.which === 13) { 
-        complete();
+        render();
     }
 });
 
+// 랜덤 번호(ID값) 생성
 function randomIdGenerate(){
     return '_' + Math.random().toString(36).substring(2,9)
 }
 
-function complete(){
+function render(){
+   //객체생성
    let task = {
         id: randomIdGenerate(),
         taskContent: taskInput.val()
-   } 
-   console.log(task);
+   }
+   //가져온 값 taskItem에 넣고 보드에 뿌려주기
    let taskItem = getTaskItem(task);
    $("#task-board").append(taskItem);
+   //인풋필드 비우기
    taskInput.val("");
 }
 
+// 입력값 가져오기
 function getTaskItem(task){
     let item=
     `<div class="task" id="taskItem-${task.id}">
@@ -35,9 +40,9 @@ function getTaskItem(task){
 }
 
 function toggleComplete(id){
-    
+    // id값이 동일한 아이템 선택
     let Status = $(`#taskItem-${id}`);
-
+    // css적용
     if(Status.hasClass("task-done")){ 
         Status.removeClass("task-done");
     } else {
